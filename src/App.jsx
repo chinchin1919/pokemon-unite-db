632d7423026a6e8b7882e5b62827d633ad90e43e
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-10-12 12:33:52 +0900

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import './App.css';
 import SignIn from './components/signin';
 import { UserInfoProvider } from './components/userInfo.jsx';
 import { LocalStorageProvider } from './components/localStorage.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <LocalStorageProvider>
       <UserInfoProvider>
         <div className="App">
-          <SignIn />
+          <BrowserRouter>
+            <Switch>
+              <SignIn />
+            </Switch>
+          </BrowserRouter>
         </div>
       </UserInfoProvider>
     </LocalStorageProvider>
