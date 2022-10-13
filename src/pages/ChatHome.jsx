e86689b456d97b6b6f10497fe36003bb446cf53d
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-10-13 11:39:16 +0900

    1 file modified

--- src/pages/ChatHome.jsx
+++ src/pages/ChatHome.jsx
@@ -2,4 +2,10 @@ const Chat = () => {
   return <>This page is not imprimented!</>;
 };
 
+const getChatRoom = async () => {
+  const res = await fetch();
+  const room = await res.json();
+  return await room;
+};
+
 export default Chat;
