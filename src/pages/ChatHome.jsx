abf65237d646ef736f1d1207fc126f6bc714a91f
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-10-13 16:09:51 +0900

    1 file modified, 1 added

--- src/pages/ChatHome.jsx
+++ src/pages/ChatHome.jsx
@@ -7,19 +7,19 @@ const getChatRoom = () => {
   let chatRoom = {};
   const beWrapped = () => {
     const res = fetch(
-      'https://script.googleusercontent.com/a/macros/f-sapporo.ed.jp/echo?user_content_key=hu0dAmzw-rTt1h9EX_LURVq91fWcmCTVZqnqipAdzvx_XwASidJ5nxWu9-XMFGW88UWLnhN7NTlfvPsAim-eUG3fWq7la9ajOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKD69xnUh-IhrsfB63eL3TaQ1djiqESLJYkXNcZoe1zwbJVFMimvbwDGwkl1xMkIUh9GcDqYzi69rNwOzl2svdDzwiMeXCzRPHnYlXVu6ZSvRTMBkFmQ3nSn6aEFGJJrzo5cy6pr-Oj0FQ&lib=McovRLwPv54_SYyI5iluMo5-ZE1Y-KuHf'
+      'https://script.googleusercontent.com/a/macros/f-sapporo.ed.jp/echo?user_content_key=BH8Kv2-HKQYYjlY7IqN45V38lEL1SzoVswu9259GzCNqdZheAUGElDH3UAFpbK1dMbrZjzC6hxqRxGzjnLYTBDnLNLb8gvf3OJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKD69xnUh-IhrsfB63eL3TaQ1djiqESLJYkXNcZoe1zwbJVFMimvbwDGwkl1xMkIUh9GcDqYzi69rNwOzl2svdDzwiMeXCzRPHnYlXVu6ZSvRTMBkFmQ3nSn6aEFGJJrzo5cy6pr-Oj0FQ&lib=McovRLwPv54_SYyI5iluMo5-ZE1Y-KuHf',
+      { method: 'GET', mode: 'no-cors', 'Access‐Control‐Allow‐Origin': '*' }
     )
       .then((response) => {
-        if (!response.ok) {
-          throw new Error();
-        }
-        return response.blob(); // あるいは response.json()
+        return response; // あるいは response.json()
       })
       .then((blob) => {
+        console.log(blob);
         chatRoom = JSON.stringify(blob);
       });
   };
   beWrapped();
+  console.log(JSON.stringify(chatRoom));
   return chatRoom;
 };
 
