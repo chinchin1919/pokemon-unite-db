3f7d63e60d26b4dcd32aa34fcb3036b14fb400f1
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-10-13 15:45:43 +0900

    1 file modified

--- src/pages/ChatHome.jsx
+++ src/pages/ChatHome.jsx
@@ -1,4 +1,5 @@
 const Chat = () => {
+  getChatRoom();
   return <>This page is not imprimented!</>;
 };
 
@@ -6,7 +7,7 @@ const getChatRoom = () => {
   let chatRoom = {};
   const beWrapped = () => {
     const res = fetch(
-      'https://script.google.com/a/macros/f-sapporo.ed.jp/s/AKfycbyGRApn5hMMSRMsCX3rmuQHv9EQ8QTZE9Sh7uFnuCXxhcGqgEA5v2ChsjDzqFeNXCtMKQ/exec'
+      'https://script.googleusercontent.com/a/macros/f-sapporo.ed.jp/echo?user_content_key=hu0dAmzw-rTt1h9EX_LURVq91fWcmCTVZqnqipAdzvx_XwASidJ5nxWu9-XMFGW88UWLnhN7NTlfvPsAim-eUG3fWq7la9ajOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKD69xnUh-IhrsfB63eL3TaQ1djiqESLJYkXNcZoe1zwbJVFMimvbwDGwkl1xMkIUh9GcDqYzi69rNwOzl2svdDzwiMeXCzRPHnYlXVu6ZSvRTMBkFmQ3nSn6aEFGJJrzo5cy6pr-Oj0FQ&lib=McovRLwPv54_SYyI5iluMo5-ZE1Y-KuHf'
     )
       .then((response) => {
         if (!response.ok) {
