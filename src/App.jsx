a06b83c926d2f524ca8eef721ee6a410f3acb8e7
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-10-13 09:06:54 +0900

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import SignIn from './components/signin';
 import { UserInfoProvider, useUserInfoContext } from './components/userInfo.jsx';
 import { LocalStorageProvider } from './components/localStorage.jsx';
-import { BrowserRouter Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   const { isLogin } = useUserInfoContext();
@@ -13,8 +13,9 @@ function App() {
         <div className="App">
           <BrowserRouter>
             <Routes>
-              <Route exact path={'/'} element={isLogin ? <Home /> : <Navigate replace to='/signin'} />
-              <Route path={'/signin'} element={isLogin ? <Navigate replace to='/' /> : <SignIn />}/>
+              <Route exact path='/' element={isLogin ? <Home /> : <Navigate replace to='/signin' />} />
+              <Route path='/signin' element={isLogin ? <Navigate replace to='/' /> : <SignIn />}/>
+              <Route path='*' element={<Navigate replace to='/' />}>
             </Routes>
           </BrowserRouter>
         </div>
