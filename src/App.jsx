18fafa3405332200e4c2c90f9ddda98dc9891d1b
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2022-10-13 09:20:32 +0900

    2 files modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,29 @@
 import { useState, createContext } from 'react';
 import './App.css';
 import SignIn from './components/signin';
-import {
-  UserInfoProvider,
-  useUserInfoContext,
-} from './components/userInfo.jsx';
-import { LocalStorageProvider } from './components/localStorage.jsx';
+import { useUserInfoContext } from './components/userInfo.jsx';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   const { isLogin } = useUserInfoContext();
   return (
-    <LocalStorageProvider>
-      <UserInfoProvider>
-        <div className="App">
-          <BrowserRouter>
-            <Routes>
-              <Route
-                exact
-                path="/"
-                element={isLogin ? <Home /> : <Navigate replace to="/signin" />}
-              />
-              <Route
-                path="/signin"
-                element={isLogin ? <Navigate replace to="/" /> : <SignIn />}
-              />
-              <Route path="*" element={<Navigate replace to="/" />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-      </UserInfoProvider>
-    </LocalStorageProvider>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={isLogin ? <Home /> : <Navigate replace to="/signin" />}
+          />
+          <Route
+            path="/signin"
+            element={isLogin ? <Navigate replace to="/" /> : <SignIn />}
+          />
+          {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { UserInfoProvider } from './components/userInfo.jsx';
+import { LocalStorageProvider } from './components/localStorage.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LocalStorageProvider>
+      <UserInfoProvider>
+        <App />
+      </UserInfoProvider>
+    </LocalStorageProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
